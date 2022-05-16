967217fd03bf617778dc5b39fbcde74e1273b1e4
KernYoung <95668317+KernYoung@users.noreply.github.com>
2022-05-16 14:39:34 +0800

    交接

--- src/components/header.vue
+++ src/components/header.vue
@@ -412,8 +412,8 @@
         }else if(command == 13){
           this.$router.push({ path: '/zxbMessageList'})
         }else if (command == 14){
-          this.$router.push({ path: '/ClientNoMaintain'})
-          // this.goCreditCodeMaintenance()
+          // this.$router.push({ path: '/ClientNoMaintain'})
+          this.goCreditCodeMaintenance()
         }else if(command == 15){
           this.$router.push({ path: '/tycPointsDistribute'})
         }else if(command == 16){
